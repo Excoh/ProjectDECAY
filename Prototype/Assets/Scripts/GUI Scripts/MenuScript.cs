e458cbc965bab0ffd3f275e97af342f49022b059
etang4 <etang4@asu.edu>
2015-04-19 10:38:19 -0700

    More updates Sunday Morning...

---
+++ Prototype/Assets/Scripts/GUI Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void PlayGame(){
+		Application.LoadLevel("Game");
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
